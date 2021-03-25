8ca2357765e09313bf0be15806ad6d985f3e7331
Ivana Yegros Kubicka <ivanamyk@gmail.com>
2021-03-25 15:34:06 -0300

    creo card

--- web-movies/src/components/Main/components/Card/index.tsx
+++ web-movies/src/components/Main/components/Card/index.tsx
@@ -1,10 +1,12 @@
 import React, { FC } from 'react'
+import CardImg from '../../../../assets/img/zelda.jpg'
+
 
 const Card: FC = () => {
     return (
         <div>
-            <div className="card bg-dark text-white">
-                <img src="" className="card-img" alt="..."></img>
+            <div className="card text-dark">
+                <img src={CardImg} className="card-img" alt="zelda" height='100px'></img>
                 <div className="card-img-overlay">
                     <h5 className="card-title">Card title</h5>
                     <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
--- web-movies/src/components/Main/index.tsx
+++ web-movies/src/components/Main/index.tsx
@@ -1,10 +1,13 @@
 import React, { FC } from "react";
+import { Card } from './components/Card'
 import "./main.css";
 
 const Main: FC = () => {
     return (
         <>
-            <main className='main-pg'></main>
+            <main className='main-pg'>
+                <Card />
+            </main>
         </>
     );
 };
