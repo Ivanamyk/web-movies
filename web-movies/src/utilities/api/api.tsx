9d4e6e423b6c928ca244d1616a396101d7e2e499
Ivana Yegros Kubicka <ivanamyk@gmail.com>
2021-03-25 19:42:44 -0300

    haciendo utils

--- web-movies/src/utilities/api/api.tsx
+++ web-movies/src/utilities/api/api.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
 const api = axios.create({
-    baseURL: "",
+    baseURL: "https://api.themoviedb.org/3/movie/550?api_key=37a3fe6b90b313baf3dc9a073da27c36",
 });
 export { api };
