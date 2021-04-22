06a1a282da4b582ef991992f7236be90ba1224df
Ivana Yegros Kubicka <ivanamyk@gmail.com>
2021-04-22 20:24:13 -0300

    finalizo tp

--- web-movies/src/screens/Details/components/Spinner/index.tsx
+++ web-movies/src/screens/Details/components/Spinner/index.tsx
@@ -4,7 +4,7 @@ import Loader from "react-loader-spinner";
 const Spinner = () => {
     return (
         <div className='d-flex justify-content-center text-warning m-5'>
-            <Loader type="Puff" color="#00BFFF" height={100} width={100} timeout={3000}>Loading...</Loader>
+            <Loader type="Puff" color="#00BFFF" height={100} width={100} timeout={3000} />
         </div>
     )
 }
