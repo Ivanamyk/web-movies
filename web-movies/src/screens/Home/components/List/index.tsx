c43cc0fe2324003c0559e3d5576356787cd138c0
Ivana Yegros Kubicka <ivanamyk@gmail.com>
2021-04-26 23:11:03 -0300

    terminado home update

--- web-movies/src/screens/Home/components/List/index.tsx
+++ web-movies/src/screens/Home/components/List/index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState, FC } from 'react'
 import { MovieType } from '../../../../types'
 import { movie } from './../../../../api/movies'
+import * as Icon from 'react-bootstrap-icons'
 import './list.css'
 
 const List: FC = () => {
@@ -19,38 +20,90 @@ const List: FC = () => {
         })
     }, []);
 
+
     const imgBase = "https://image.tmdb.org/t/p/"
     const imgWidth = "w500"
 
+    const section1 = popularMovies && popularMovies.slice(0, 9)
+    const section2 = popularMovies && popularMovies.slice(11, 20)
+
+    const trSection1 = topRated && topRated.slice(0, 9)
+    const trSection2 = topRated && topRated.slice(11, 20)
+
     return (
-        <div className='row justify-content-center mt-5'>
-            <div className='col-4 list-table'>
-                <ul className="list-group main-list">
-                    <h4 className="list-group-item header-list">Peliculas Populares</h4>
-                    <div className='scroll'>
-                        {popularMovies && popularMovies.map((movie: MovieType) => (
-                            <li className="list-group-item movie-item">
-                                <img src={imgBase + imgWidth + movie.poster_path} className='list-img' alt="" /><a className='movie-name' href={"/details/" + movie.id} >{movie.title}</a>
-                            </li>
-                        ))
-                        }
+        <>
+            <section className='section1'>
+                <div id="carouselExampleControls" className="carousel slide" data-interval="false">
+                    <h4 className="slider-header">Top Rated</h4>
+                    <div className="carousel-inner">
+                        <div className="carousel-item active">
+                            <ul className="container-slider">
+                                {trSection1 && trSection1.map((movie: MovieType) => (
+                                    <a className="item-slider" href={"/details/" + movie.id}>
+                                        <img src={imgBase + imgWidth + movie.poster_path} className='movie-img' />
+                                    </a>
+                                ))
+                                }
+                            </ul>
+                        </div>
+                        <div className="carousel-item">
+                            <ul className="container-slider active">
+                                {trSection2 && trSection2.map((movie: MovieType) => (
+                                    <a className="item-slider" href={"/details/" + movie.id}>
+                                        <img src={imgBase + imgWidth + movie.poster_path} className='movie-img' />
+                                    </a>
+                                ))
+                                }
+                            </ul>
+                        </div>
                     </div>
-                </ul>
-            </div>
-            <div className='col-4 list-table'>
-                <ul className="list-group main-list">
-                    <h4 className="list-group-item header-list">Peliculas Mejor Puntuadas</h4>
-                    <div className='scroll'>
-                        {topRated && topRated.map((movie: MovieType) => (
-                            <li className="list-group-item movie-item">
-                                <img src={imgBase + imgWidth + movie.poster_path} className='list-img' alt="" /><a className='movie-name' href={"/details/" + movie.id}>{movie.title}</a>
-                            </li>
-                        ))
-                        }
+                    <div className='container container-btns'>
+                        <button className="slider-btn" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                            <span aria-hidden="true"><Icon.ArrowLeftCircle className='arrow fa-2x' /></span>
+                        </button>
+                        <button className="slider-btn" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                            <span aria-hidden="true"><Icon.ArrowRightCircle className='arrow fa-2x' /></span>
+                        </button>
                     </div>
-                </ul>
-            </div>
-        </div>
+                </div>
+            </section>
+            <section className='section2'>
+                <div id="carouselExampleControls2" className="carousel slide" data-interval="false">
+                    <h4 className="slider-header">Most Popular</h4>
+                    <div className="carousel-inner">
+                        <div className="carousel-item active">
+                            <ul className="container-slider">
+                                {section1 && section1.map((movie: MovieType) => (
+                                    <a className="item-slider" href={"/details/" + movie.id}>
+                                        <img src={imgBase + imgWidth + movie.poster_path} className='movie-img' />
+                                    </a>
+                                ))
+                                }
+                            </ul>
+                        </div>
+                        <div className="carousel-item">
+                            <ul className="container-slider active">
+                                {section2 && section2.map((movie: MovieType) => (
+                                    <a className="item-slider" href={"/details/" + movie.id}>
+                                        <img src={imgBase + imgWidth + movie.poster_path} className='movie-img' />
+                                    </a>
+                                ))
+                                }
+                            </ul>
+                        </div>
+                    </div>
+                    <div className='container container-btns'>
+                        <button className="slider-btn" type="button" data-bs-target="#carouselExampleControls2" data-bs-slide="prev">
+                            <span aria-hidden="true"><Icon.ArrowLeftCircle className='arrow2 fa-2x' /></span>
+                        </button>
+                        <button className="slider-btn" type="button" data-bs-target="#carouselExampleControls2" data-bs-slide="next">
+                            <span aria-hidden="true"><Icon.ArrowRightCircle className='arrow2 fa-2x' /></span>
+                        </button>
+                    </div>
+                </div>
+            </section>
+
+        </>
     )
 }
 export { List }
